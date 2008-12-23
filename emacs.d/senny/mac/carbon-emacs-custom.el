082f896d44ee762018529e1998ad0afe52c8bdb0
Yves Senn <yves.senn@gmail.com>
2008-12-23 17:57:47 +0100

    added faces for diff and ediff

--- emacs.d/senny/mac/carbon-emacs-custom.el
+++ emacs.d/senny/mac/carbon-emacs-custom.el
@@ -9,15 +9,13 @@
  '(jde-complete-insert-method-signature nil t)
  '(jde-jdk (quote ("1.5")) t)
  '(jde-jdk-registry (quote (("1.5" . "/System/Library/Frameworks/JavaVM.framework/Versions/1.5"))) t)
- '(mf-display-padding-height 120)
- '(transient-mark-mode t))
+ '(mf-display-padding-height 120))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
-
+)
 ;; for compatibility with older Aquamacs versions
  (defvar aquamacs-140-custom-file-upgraded t)
  (unless (fboundp 'auto-detect-longlines) (defun auto-detect-longlines () t))
