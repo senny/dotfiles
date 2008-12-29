dfd3e52ffd4d98ed6ccdbda7771269152ac416ad
Yves Senn <yves.senn@gmail.com>
2008-12-29 23:36:14 +0100

    added nxhtml-mode

---
+++ emacs.d/vendor/nxhtml/tests/temp-test.el
@@ -0,0 +1,6 @@
+(setq debug-on-error t)
+(eval-when-compile (require 'cl))
+(delete-other-windows)
+(eval-after-load 'nxhtml '(setq nxhtml-skip-welcome t))
+(setq nxhtmltest-default-fontification-method 'fontify-w-timer-handlers)
+(pushnew "c:/emacs/p/081219/emacs/lisp/progmodes/" load-path)
