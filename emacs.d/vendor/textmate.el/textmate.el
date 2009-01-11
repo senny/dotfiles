7510c259ee5428b61211f3e0a293d48ab6d5b722
Yves Senn <yves.senn@gmail.com>
2009-01-11 15:08:52 +0100

    changed textmate.el

--- emacs.d/vendor/textmate.el/textmate.el
+++ emacs.d/vendor/textmate.el/textmate.el
@@ -137,6 +137,24 @@
   (apply (symbol-function reading-fn) args)))
 
 ;;; Commands
+(defun comment-or-uncomment-line (&optional lines)
+  "Comment current line. Argument gives the number of lines
+forward to comment"
+  (interactive "P")
+  (comment-or-uncomment-region
+   (line-beginning-position)
+   (line-end-position lines)))
+
+(defun comment-or-uncomment-region-or-line (&optional lines)
+  "If the line or region is not a comment, comments region
+if mark is active, line otherwise. If the line or region
+is a comment, uncomment."
+  (interactive "P")
+  (if mark-active
+      (if (< (mark) (point))
+          (comment-or-uncomment-region (mark) (point))
+        (comment-or-uncomment-region (point) (mark)))
+    (comment-or-uncomment-line lines)))
 
 (defun textmate-next-line ()
   (interactive)
