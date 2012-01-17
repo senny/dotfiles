cdf9eae4f43830437c01118a9f411123983c00d6
Yves Senn <yves.senn@garaio.com>
2012-01-17 13:12:33 +0100

    personal emacs additions

---
+++ emacs.d/local.el
@@ -0,0 +1,24 @@
+(add-hook 'e-max-initialized-hook (lambda ()
+                                    (setq rspec-use-bundler-when-possible t)))
+
+(defun senny-tableize-data ()
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (replace-string "	" "|")
+    (beginning-of-buffer)
+    (replace-regexp "$" "|")
+    (beginning-of-buffer)
+    (replace-regexp "^" "|")
+    (orgtbl-mode)
+    (call-interactively 'orgtbl-tab)))
+
+(global-set-key (kbd "M-<prior>") 'start-kbd-macro)
+(global-set-key (kbd "M-<next>") 'end-kbd-macro)
+
+(defun senny-toggle-rspec-use-bundler ()
+  (interactive)
+  (setq rspec-use-bundler-when-possible
+        (not rspec-use-bundler-when-possible)))
+
+(global-set-key (kbd "<f6>") 'senny-toggle-rspec-use-bundler)
